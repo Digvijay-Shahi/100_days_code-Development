f058217e63f9fab81918f534b575e203caebfe1a
DIGVIJAY SHAHI <88196191+Digvijay-Shahi@users.noreply.github.com>
2022-11-15 22:40:21 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,57 @@
+// console.log("This program adds 1, 2 and 54")
+
+// function addThreeNumbers(a, b, c) {
+//     return a + b + c;
+// }
+// let c = addThreeNumbers(1, 2, 54)
+// console.log(c)
+
+// console.log("This repl contains code for the Ultimate JavaScript course video no 2")
+// var a = 67 // a contains 67
+// console.log(a)
+// a = "harry"
+// console.log(a)
+// // let 8harry = 7 // Not allowed this will throw an error
+// // let var = 7
+// // nn bb ss u - Primitives in Js
+// let a = null;
+// let b = 345;
+// let c = true; // can also be false
+// let d = BigInt("567") + BigInt("3")
+// let e = "Harry"
+// let f = Symbol("I am a nice symbol")
+// let g
+// console.log(a, b, c , d, e, f, g)
+// console.log(typeof c)
+
+// // Non Primitive Data Type - Objects in Js
+// const item = {
+//   "Harry": true,
+//   "Shubh": false,
+//   "Lovish": 67,
+//   "Rohan": undefined
+// }
+// console.log(item["Shubh"])
+
+// //practise set 1
+
+// // console.log('hello')
+// let a= "hello"
+// let b = 5
+// console.log(a+b)
+// console.log(typeof  (a+b))
+// const shahi= {
+//         name:"Digvijay",
+//         branch: "cse",
+//         status: "single"
+//     }
+//     shahi['clg'] = "birla"
+//     console.log(shahi)
+
+//     const dir={
+//         happy:"khus",
+//         sad:"dhuk",
+//         money:"paisa",
+//         glasse:"chasma"
+//     }
+// console.log(dir.happy)
