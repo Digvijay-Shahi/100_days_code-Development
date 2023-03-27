ec0b89e50a77b6e48cabda57a21483d15db0b314
DIGVIJAY SHAHI <88196191+Digvijay-Shahi@users.noreply.github.com>
2023-03-27 23:51:20 +0530

    1 Week update
    Learned about fundamentals of javascript.

---
+++ script.js
@@ -0,0 +1,32 @@
+// let js = 'digvijay';
+// if (js === 'digvijay') 
+//     545 +3 + 88
+//   console.log(545 +3 + 884);
+
+// let h3f = 'hello';
+// console.log(h3f)
+
+ // templete literals 
+// console.log(`multiple 
+// line 
+// use`);
+// switch case 
+ const day = 'sunday'
+ if (day === "monday"){
+    console.log('its monday ')
+
+ }
+ else if(day === 'tuesday'){
+    console.log('its tuesday ')
+ }
+ else if(day === 'wednesday' || day === 'thrusday'){
+    console.log('its wednesday ')
+ }else if(day === 'friday'){
+    console.log('its friday ')
+ }
+ else if(day === 'saturday'|| day=== 'sunday'){
+    console.log('its sunday ')
+ }
+ else {
+    console.log('its wrong day')
+ }
